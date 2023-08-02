db6cb8c8e3756364892b7d04631b9a43e1dec773
Serena <serena.s.xu@gmail.com>
2023-08-01 19:04:21 -0700

    added bg images but no work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,60 @@
-import * as React from "react";
+import React, { useState } from "react"; 
 import { StaticImage } from "gatsby-plugin-image";
-
 import "../css/index.css";
 
+const sections = [
+  {
+    title: "Section 1",
+    image: "../images/aaf-comp.jpg", // Replace with actual image path
+  },
+  {
+    title: "Section 2",
+    image: "../images/aaf-comp.jpg", // Replace with actual image path
+  },
+  {
+    title: "Section 3",
+    image: "../images/aaf-comp.jpg", // Replace with actual image path
+  },
+  // Add more sections as needed
+];
+
 const IndexPage = () => {
+  const [backgroundImage, setBackgroundImage] = React.useState("");
+
+  const handleSectionHover = (image) => {
+    setBackgroundImage(image);
+  };
+
   return (
     <main>
       <div className="left-div">
         <div className="logo-container">
-        <StaticImage
-        src="../images/sx-logo.png"
-        loading="eager"
-        width={200}
-        quality={80}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-        />
+          <StaticImage
+            src="../images/sx-logo.png"
+            loading="eager"
+            width={200}
+            quality={80}
+            formats={["auto", "webp", "avif"]}
+            alt=""
+            style={{ marginBottom: `var(--space-3)` }}
+          />
         </div>
-        
       </div>
 
-      <div className="right-div">
+      <div className="right-div" style={{ backgroundImage: `url(${backgroundImage})` }}>
         <div className="navigation-container">
           <h2 className="navigation-heading">My Work</h2>
           <nav className="vertical-nav">
             <ul>
-              <li>
-                <a href="#section1">Section 1</a>
-              </li>
-              <li>
-                <a href="#section2">Section 2</a>
-              </li>
-              <li>
-                <a href="#section3">Section 3</a>
-              </li>
-              {/* Add more navigation items as needed */}
+              {sections.map((section, index) => (
+                <li
+                  key={index}
+                  onMouseEnter={() => handleSectionHover(section.image)}
+                  onMouseLeave={() => handleSectionHover("")}
+                >
+                  <a href={`#section${index + 1}`}>{section.title}</a>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
@@ -48,7 +67,6 @@ const IndexPage = () => {
           <p>I’m a designer working on infrastructure and developer experiences.</p>
         </div>
       </div>
-
     </main>
   );
 };
